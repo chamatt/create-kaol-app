54b84e8023ddfd7088f942498bb981a80c98b91d
chamatt <matt.vicent@gmail.com>
2022-08-10 04:11:59 -0300

    don't show env flag on web version

--- packages/app/components/EnvironmentStatusBar/index.tsx
+++ packages/app/components/EnvironmentStatusBar/index.tsx
@@ -1,12 +1,16 @@
 import { getCurrentChannel } from 'app/config'
+import { Platform } from 'react-native'
 import { Text, View } from 'universal'
 
 export const EnvironmentStatusBar = () => {
-  return (
-    <View tw="bg-orange-200 text-center">
-      <Text tw="text-gray-600 text-center">
-        Channel: {getCurrentChannel?.()}
-      </Text>
-    </View>
-  )
+  if (Platform.OS !== 'web') {
+    return (
+      <View tw="bg-orange-200 text-center">
+        <Text tw="text-gray-600 text-center">
+          Channel: {getCurrentChannel?.()}
+        </Text>
+      </View>
+    )
+  }
+  return null
 }
